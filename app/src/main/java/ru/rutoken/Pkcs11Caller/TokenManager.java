e1c438bc6362420b5d15a83f6b463d298edc60ff
Eugene Mironenko <mironenko@rutoken.ru>
2014-08-21 20:04:04 +0400

    Working with multiple tokens. Moving to callbacks

--- app/src/main/java/ru/rutoken/Pkcs11Caller/TokenManager.java
+++ app/src/main/java/ru/rutoken/Pkcs11Caller/TokenManager.java
@@ -37,11 +37,11 @@ public class TokenManager {
             receive(intent);
         }
     };
-    private boolean mFollowSlot = true;
-    private boolean mWaitingForToken = false;
-    private int mSlotId = -1;
-
-    private Thread mEasyHandler;
+//    private boolean mFollowSlot = true;
+//    private boolean mWaitingForToken = false;
+//    private int mSlotId = -1;
+//
+//    private Thread mEasyHandler;
 
     private void receive(Intent intent) {
         try {
@@ -67,12 +67,23 @@ public class TokenManager {
             temp.put(EventHandler.SLOT_HAS_EVENT, TokenManager.class.getDeclaredMethod("slotHasEvent", new Class[] {Intent.class}));
             temp.put(EventHandler.ENUMERATION_FINISHED, TokenManager.class.getDeclaredMethod("enumerationFinished", new Class[] {Intent.class}));
             temp.put(EventHandler.SLOT_WILL_HAVE_TOKEN, TokenManager.class.getDeclaredMethod("slotWillHaveToken", new Class[] {Intent.class}));
-            temp.put(EventHandler.SLOT_EVENT_FAILED, TokenManager.class.getDeclaredMethod("slotEventFailed", new Class[] {Intent.class}));
-            temp.put(EventHandler.EVENT_HANDLER_FAILED, TokenManager.class.getDeclaredMethod("eventHandlerFailed", new Class[] {Intent.class}));
+            //temp.put(EventHandler.SLOT_EVENT_FAILED, TokenManager.class.getDeclaredMethod("slotEventFailed", new Class[] {Intent.class}));
+            //temp.put(EventHandler.EVENT_HANDLER_FAILED, TokenManager.class.getDeclaredMethod("eventHandlerFailed", new Class[] {Intent.class}));
         } catch (NoSuchMethodException e) {
             Log.e(getClass().getName(), e.getMessage());
         }
         mEventReceivers = Collections.unmodifiableMap(temp);
+    }
+
+    public synchronized void processEvent(EventType event, int slotId, Token token) {
+        if(event == EventType.EVENT_HANDLER_FAILED) {
+            eventHandlerFailed();
+            return;
+        }
+        if(event == EventType.ENUMERATION_FINISHED) {
+            enumerationFinished();
+            return;
+        }
     }
 
     public static synchronized TokenManager getInstance() {
@@ -102,102 +113,102 @@ public class TokenManager {
 
         mEventHandler = new EventHandler(mContext);
         mEventHandler.start();
-
-        mEasyHandler = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    int rv = RtPkcs11Library.getInstance().C_Initialize(null);
-                    if (Pkcs11Constants.CKR_OK != rv) {
-                        throw Pkcs11Exception.exceptionWithCode(rv);
-                    }
-                    LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(CRYPTOKI_INITIALIZED));
-                } catch(Exception e){
-                    LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(EVENT_HANDLER_FAILED));
-                }
-
-                while (true) {
-                    try {
-                        IntByReference slotCount = new IntByReference(0);
-                        int rv = RtPkcs11Library.getInstance().C_GetSlotList(false, null, slotCount);
-                        if (Pkcs11Constants.CKR_OK != rv) {
-                            throw Pkcs11Exception.exceptionWithCode(rv);
-                        }
-                        int slotIds[] = new int[slotCount.getValue()];
-                        rv = RtPkcs11Library.getInstance().C_GetSlotList(true, slotIds, slotCount);
-                        if (Pkcs11Constants.CKR_OK != rv) {
-                            throw Pkcs11Exception.exceptionWithCode(rv);
-                        }
-                        for (int i = 0; i != slotCount.getValue(); ++i) {
-                            if(mFollowSlot && (mSlotId == -1)) slotEventHappened(i);
-                            else break;
-                        }
-                        LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(ENUMERATION_FINISHED));
-                    } catch(Exception e) {
-                        LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(EVENT_HANDLER_FAILED));
-                    }
-
-                    while (true) {
-                        IntByReference id = new IntByReference();
-                        int rv = RtPkcs11Library.getInstance().C_WaitForSlotEvent(0, id, null);
-                        if (Pkcs11Constants.CKR_CRYPTOKI_NOT_INITIALIZED == rv) {
-                            return;
-                        }
-                        try {
-                            if (Pkcs11Constants.CKR_OK != rv) {
-                                throw Pkcs11Exception.exceptionWithCode(rv);
-                            }
-                            int slotId = id.getValue();
-                            if (mFollowSlot && (mSlotId == -1)) slotEventHappened(id.getValue()); // token inserted -- no reenum
-                            else if (mFollowSlot && (mSlotId == slotId)) {
-                                slotEventHappened(id.getValue());
-                                if (mSlotId == -1) break; // token removed -- need reenum
-                            }
-                            else if (!mFollowSlot && (mSlotId == -1)) slotEventHappened(id.getValue());
-                            else if (!mFollowSlot) slotEventHappened(id.getValue());
-                        } catch (Exception e) {
-                            LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(EVENT_HANDLER_FAILED));
-                            continue;
-                        }
-                    }
-                }
-            }
-        };
+//
+//        mEasyHandler = new Thread() {
+//            @Override
+//            public void run() {
+//                try {
+//                    int rv = RtPkcs11Library.getInstance().C_Initialize(null);
+//                    if (Pkcs11Constants.CKR_OK != rv) {
+//                        throw Pkcs11Exception.exceptionWithCode(rv);
+//                    }
+//                    LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(CRYPTOKI_INITIALIZED));
+//                } catch(Exception e){
+//                    LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(EVENT_HANDLER_FAILED));
+//                }
+//
+//                while (true) {
+//                    try {
+//                        IntByReference slotCount = new IntByReference(0);
+//                        int rv = RtPkcs11Library.getInstance().C_GetSlotList(false, null, slotCount);
+//                        if (Pkcs11Constants.CKR_OK != rv) {
+//                            throw Pkcs11Exception.exceptionWithCode(rv);
+//                        }
+//                        int slotIds[] = new int[slotCount.getValue()];
+//                        rv = RtPkcs11Library.getInstance().C_GetSlotList(true, slotIds, slotCount);
+//                        if (Pkcs11Constants.CKR_OK != rv) {
+//                            throw Pkcs11Exception.exceptionWithCode(rv);
+//                        }
+//                        for (int i = 0; i != slotCount.getValue(); ++i) {
+//                            if(mFollowSlot && (mSlotId == -1)) slotEventHappened(i);
+//                            else break;
+//                        }
+//                        LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(ENUMERATION_FINISHED));
+//                    } catch(Exception e) {
+//                        LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(EVENT_HANDLER_FAILED));
+//                    }
+//
+//                    while (true) {
+//                        IntByReference id = new IntByReference();
+//                        int rv = RtPkcs11Library.getInstance().C_WaitForSlotEvent(0, id, null);
+//                        if (Pkcs11Constants.CKR_CRYPTOKI_NOT_INITIALIZED == rv) {
+//                            return;
+//                        }
+//                        try {
+//                            if (Pkcs11Constants.CKR_OK != rv) {
+//                                throw Pkcs11Exception.exceptionWithCode(rv);
+//                            }
+//                            int slotId = id.getValue();
+//                            if (mFollowSlot && (mSlotId == -1)) slotEventHappened(id.getValue()); // token inserted -- no reenum
+//                            else if (mFollowSlot && (mSlotId == slotId)) {
+//                                slotEventHappened(id.getValue());
+//                                if (mSlotId == -1) break; // token removed -- need reenum
+//                            }
+//                            else if (!mFollowSlot && (mSlotId == -1)) slotEventHappened(id.getValue());
+//                            else if (!mFollowSlot) slotEventHappened(id.getValue());
+//                        } catch (Exception e) {
+//                            LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(EVENT_HANDLER_FAILED));
+//                            continue;
+//                        }
+//                    }
+//                }
+//            }
+//        };
     }
 
-    protected void slotEventHappened(int id) throws Pkcs11Exception{
-        CK_SLOT_INFO slotInfo = new CK_SLOT_INFO();
-        int rv = RtPkcs11Library.getInstance().C_GetSlotInfo(id, slotInfo);
-        if (Pkcs11Constants.CKR_OK != rv) {
-            throw Pkcs11Exception.exceptionWithCode(rv);
-        }
-        CK_TOKEN_INFO tokenInfo = new CK_TOKEN_INFO();
-        CK_TOKEN_INFO_EXTENDED extendedInfo = new CK_TOKEN_INFO_EXTENDED();
-        extendedInfo.ulSizeofThisStructure = extendedInfo.size();
-
-        Intent dict = new Intent();
-        dict.putExtra("slotId", id).putExtra("slotInfo", new SlotInfo(slotInfo));
-
-        if ((Pkcs11Constants.CKF_TOKEN_PRESENT & slotInfo.flags) != 0x00) {
-            LocalBroadcastManager.getInstance(mContext).sendBroadcast(dict.setAction(SLOT_WILL_HAVE_TOKEN));
-            try {
-                rv = RtPkcs11Library.getInstance().C_GetTokenInfo(id, tokenInfo);
-                if (Pkcs11Constants.CKR_OK != rv) {
-                    throw Pkcs11Exception.exceptionWithCode(rv);
-                }
-                rv = RtPkcs11Library.getInstance().C_EX_GetTokenInfoExtended(id, extendedInfo);
-                if (Pkcs11Constants.CKR_OK != rv) {
-                    throw Pkcs11Exception.exceptionWithCode(rv);
-                }
-            } catch (Exception e) {
-                LocalBroadcastManager.getInstance(mContext).sendBroadcast(dict.setAction(SLOT_EVENT_FAILED));
-                return;
-            }
-        }
-        dict.putExtra("tokenInfo", new TokenInfo(tokenInfo));
-        dict.putExtra("extendedTokenInfo", new TokenInfoEx(extendedInfo));
-        LocalBroadcastManager.getInstance(mContext).sendBroadcast(dict.setAction(SLOT_HAS_EVENT));
-    }
+//    protected void slotEventHappened(int id) throws Pkcs11Exception{
+//        CK_SLOT_INFO slotInfo = new CK_SLOT_INFO();
+//        int rv = RtPkcs11Library.getInstance().C_GetSlotInfo(id, slotInfo);
+//        if (Pkcs11Constants.CKR_OK != rv) {
+//            throw Pkcs11Exception.exceptionWithCode(rv);
+//        }
+//        CK_TOKEN_INFO tokenInfo = new CK_TOKEN_INFO();
+//        CK_TOKEN_INFO_EXTENDED extendedInfo = new CK_TOKEN_INFO_EXTENDED();
+//        extendedInfo.ulSizeofThisStructure = extendedInfo.size();
+//
+//        Intent dict = new Intent();
+//        dict.putExtra("slotId", id).putExtra("slotInfo", new SlotInfo(slotInfo));
+//
+//        if ((Pkcs11Constants.CKF_TOKEN_PRESENT & slotInfo.flags) != 0x00) {
+//            LocalBroadcastManager.getInstance(mContext).sendBroadcast(dict.setAction(SLOT_WILL_HAVE_TOKEN));
+//            try {
+//                rv = RtPkcs11Library.getInstance().C_GetTokenInfo(id, tokenInfo);
+//                if (Pkcs11Constants.CKR_OK != rv) {
+//                    throw Pkcs11Exception.exceptionWithCode(rv);
+//                }
+//                rv = RtPkcs11Library.getInstance().C_EX_GetTokenInfoExtended(id, extendedInfo);
+//                if (Pkcs11Constants.CKR_OK != rv) {
+//                    throw Pkcs11Exception.exceptionWithCode(rv);
+//                }
+//            } catch (Exception e) {
+//                LocalBroadcastManager.getInstance(mContext).sendBroadcast(dict.setAction(SLOT_EVENT_FAILED));
+//                return;
+//            }
+//        }
+//        dict.putExtra("tokenInfo", new TokenInfo(tokenInfo));
+//        dict.putExtra("extendedTokenInfo", new TokenInfoEx(extendedInfo));
+//        LocalBroadcastManager.getInstance(mContext).sendBroadcast(dict.setAction(SLOT_HAS_EVENT));
+//    }
 
     public synchronized void destroy() {
         if(null == mContext)
@@ -216,9 +227,10 @@ public class TokenManager {
         mContext = null;
     }
 
-    private void enumerationFinished(Intent intent) {
+    private void enumerationFinished() {
         LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(ENUMERATION_FINISHED));
     };
+
     private synchronized void slotHasEvent(Intent intent) {
         int slotId = intent.getIntExtra("slotId", -1);
         SlotInfo slotInfo = intent.getParcelableExtra("slotInfo");
@@ -246,7 +258,7 @@ public class TokenManager {
     private void slotEventFailed(Intent intent) {
         LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(TOKEN_ADDING_FAILED));
     };
-    private void eventHandlerFailed(Intent intent) {
+    private void eventHandlerFailed() {
         LocalBroadcastManager.getInstance(mContext).sendBroadcast(new Intent(INTERNAL_ERROR));
     };
 
