66c7dd4757cdaf0974356a984790b60e973cadde
Maxim Sergeev <sergeev@rutoken.ru>
2023-08-23 13:33:10 +0300

    Fix deprecation warnings
    * Add queries with Control Panel package name in manifest, because apps
    that target Android 11 cannot query or interact with other installed
    apps by default;
    * Change PackageManager#getInstalledApplications to
    PackageManager#getPackageInfo method call on devices with SDK version R
    and upper;
    * Remove deprecated package parameter from manifest and add namespace in
    build.gradle instead;
    * Change deprecated compileSdkVersion, minSdkVersion and targetSdkVersion
    to compileSdk, minSdk, targetSdk in build.gradle.

--- app/src/main/java/ru/rutoken/demobank/utils/PcscChecker.java
+++ app/src/main/java/ru/rutoken/demobank/utils/PcscChecker.java
@@ -1,6 +1,9 @@
 package ru.rutoken.demobank.utils;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
@@ -16,18 +19,38 @@ public final class PcscChecker {
     }
 
     public static void checkPcscInstallation(FragmentActivity activity) {
-        List<ApplicationInfo> packages = activity.getPackageManager().getInstalledApplications(0);
-        boolean pcscInstalled = false;
-        for (ApplicationInfo packageInfo : packages) {
-            if (packageInfo.packageName.equals(PCSC_PACKAGE_NAME)) {
-                pcscInstalled = true;
-                break;
-            }
-        }
-
-        if (!pcscInstalled) {
+        if (!isRutokenPanelInstalled(activity)) {
             DialogFragment newFragment = new PcscInstallDialogFragment();
             newFragment.show(activity.getSupportFragmentManager(), "PcscInstallFragment");
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private static boolean isRutokenPanelInstalled(FragmentActivity activity) {
+        PackageManager packageManager = activity.getPackageManager();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    packageManager.getPackageInfo(PCSC_PACKAGE_NAME,
+                            PackageManager.PackageInfoFlags.of(0));
+                } else {
+                    packageManager.getPackageInfo(PCSC_PACKAGE_NAME, 0);
+                }
+                return true;
+            } catch (PackageManager.NameNotFoundException e) {
+                return false;
+            }
+        } else {
+            boolean pcscInstalled = false;
+            List<ApplicationInfo> packages = packageManager.getInstalledApplications(0);
+            for (ApplicationInfo packageInfo : packages) {
+                if (packageInfo.packageName.equals(PCSC_PACKAGE_NAME)) {
+                    pcscInstalled = true;
+                    break;
+                }
+            }
+
+            return pcscInstalled;
         }
     }
 }
